ea18edb7f1736119c857e1b380a9dc6886f84368
lschlestein <lschlestein@gmail.com>
2023-05-26 20:24:34 -0300

    Implemntado código para listar e cadastrar produtos

--- SpringBootProduto/src/main/java/com/produto/produto/ProdutoController.java
+++ SpringBootProduto/src/main/java/com/produto/produto/ProdutoController.java
@@ -1,7 +1,13 @@
 package com.produto.produto;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProdutoController {
@@ -12,5 +18,25 @@ public class ProdutoController {
 	}
 
 	private ProdutoRepository produtoRepository;
+	
+	@GetMapping("/listar")
+	public String listarProduto(Model model) {
+		List<Produto> lista = produtoRepository.findAll();
+		model.addAttribute("listaProdutos",lista);
+		return "listarProdutos";
+	}
+	
+	@GetMapping("/cadastrar")
+	public String cadastrarProduto(Model model) {
+		model.addAttribute("produto", new Produto());
+		return "cadastrarProduto";
+	}
+	
+	@PostMapping("/cadastrar")
+	public String addProduto(@ModelAttribute Produto produto, Model model) {
+		produtoRepository.save(produto);
+		listarProduto(model);
+		return "listarProdutos";
+	}
 
 }
--- SpringBootProduto/src/main/java/com/produto/produto/ProdutoRepository.java
+++ SpringBootProduto/src/main/java/com/produto/produto/ProdutoRepository.java
@@ -2,6 +2,6 @@ package com.produto.produto;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProdutoRepository extends JpaRepository<Produto, Long>{
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 
 }
